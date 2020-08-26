0208d6ffbb079d86fa3d0887adcf1339cedd4c6d
Andre Noberto <andrenoberto@protonmail.com>
2020-08-26 19:23:39 -0300

    Getting values from .env file

---
+++ src/config/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const getEnv = require('getenv');
+
+const registrationOffice = {
+  action: getEnv.string('REGISTRATION_OFFICE_API_ACTION'),
+  address: getEnv.string('REGISTRATION_OFFICE_ADDRESS'),
+  category: getEnv.string('REGISTRATION_OFFICE_CATEGORY'),
+  endpoint: getEnv.string('REGISTRATION_OFFICE_API_ENDPOINT'),
+  subCategory: getEnv.string('REGISTRATION_OFFICE_SUB_CATEGORY'),
+  type: getEnv.string('REGISTRATION_OFFICE_TYPE'),
+};
+
+const twilio = {
+  accountSid: getEnv.string('TWILIO_ACCOUNT_SID'),
+  authToken: getEnv.string('TWILIO_AUTH_TOKEN'),
+  receiverNumber: getEnv.string('TWILIO_RECEIVER_NUMBER'),
+  senderNumber: getEnv.string('TWILIO_SENDER_NUMBER'),
+};
+
+module.exports = {
+  registrationOffice,
+  twilio,
+};
